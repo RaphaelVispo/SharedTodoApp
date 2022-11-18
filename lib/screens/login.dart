193e380afc3fbae154bcc4886fba5c3e4c6da46e
RaphaelVispo <rsvispo@up.edu.ph>
2022-11-18 08:37:08 +0800

    feat: validator login

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -10,26 +10,54 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     TextEditingController emailController = TextEditingController();
     TextEditingController passwordController = TextEditingController();
 
-    final email = TextField(
+    final email = TextFormField(
       key: const Key('emailField'),
       controller: emailController,
       decoration: const InputDecoration(
         hintText: "Email",
       ),
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Please enter some text';
+        }
+        if (!value.contains('@')) {
+          return 'Email is invalid, must contain @';
+        }
+        if (!value.contains('.')) {
+          return 'Email is invalid, must contain .';
+        }
+        return null;
+      },
+      onChanged: (value) {
+        _formKey.currentState!.validate();
+      },
     );
 
-    final password = TextField(
+    final password = TextFormField(
       key: const Key('pwField'),
       controller: passwordController,
       obscureText: true,
       decoration: const InputDecoration(
         hintText: 'Password',
       ),
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Please enter some text';
+        }
+        if (value.length < 7) {
+          return 'The length should be more than 6';
+        }
+        return null;
+      },
+      onChanged: (value) {
+        _formKey.currentState!.validate();
+      },
     );
 
     final loginButton = Padding(
@@ -37,10 +65,11 @@ class _LoginPageState extends State<LoginPage> {
       padding: const EdgeInsets.symmetric(vertical: 16.0),
       child: ElevatedButton(
         onPressed: () {
-         context
-          .read<AuthProvider>()
-          .signIn(emailController.text, passwordController.text);
-
+          if (_formKey.currentState!.validate()) {
+            context
+                .read<AuthProvider>()
+                .signIn(emailController.text, passwordController.text);
+          }
         },
         child: const Text('Log In', style: TextStyle(color: Colors.white)),
       ),
@@ -51,9 +80,6 @@ class _LoginPageState extends State<LoginPage> {
       padding: const EdgeInsets.symmetric(vertical: 16.0),
       child: ElevatedButton(
         onPressed: () async {
-
- 
-
           Navigator.of(context).push(
             MaterialPageRoute(
               builder: (context) => const SignupPage(),
@@ -76,10 +102,16 @@ class _LoginPageState extends State<LoginPage> {
               textAlign: TextAlign.center,
               style: TextStyle(fontSize: 25),
             ),
-            email,
-            password,
-            loginButton,
-            signUpButton,
+            Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    email,
+                    password,
+                    loginButton,
+                    signUpButton,
+                  ],
+                ))
           ],
         ),
       ),
