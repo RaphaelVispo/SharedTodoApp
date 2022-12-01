18aedc46c2821abab8f9a8fb402fd98a7db82cfe
RaphaelVispo <rsvispo@up.edu.ph>
2022-12-01 23:59:11 +0800

    fix: ui of the drawer

--- lib/screens/todo_page.dart
+++ lib/screens/todo_page.dart
@@ -10,8 +10,10 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:week7_networking_discussion/models/todo_model.dart';
+import 'package:week7_networking_discussion/models/user_models.dart';
 import 'package:week7_networking_discussion/providers/todo_provider.dart';
 import 'package:week7_networking_discussion/providers/auth_provider.dart';
+import 'package:week7_networking_discussion/providers/user_providers.dart';
 import 'package:week7_networking_discussion/screens/FriendRequest.dart';
 import 'package:week7_networking_discussion/screens/addTodo.dart';
 import 'package:week7_networking_discussion/screens/editTodo.dart';
@@ -40,6 +42,8 @@ class _TodoPageState extends State<TodoPage> {
   Widget build(BuildContext context) {
     // access the list of todos in the provider
     Stream<QuerySnapshot> todosStream = context.watch<TodoListProvider>().todos;
+    final Future<DocumentSnapshot> userInfo =
+        context.watch<UserProvider>().info;
 
     return Scaffold(
       extendBodyBehindAppBar: true,
@@ -51,66 +55,111 @@ class _TodoPageState extends State<TodoPage> {
         elevation: 0,
       ),
       drawer: Drawer(
-          child: ListView(padding: EdgeInsets.zero, children: [
-        ListTile(
-            title: const Text('Shared Todo'),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const sharedTodo()),
-              );
-            }),
-        ListTile(title: const Text('Profile'), onTap: () {
+          child: FutureBuilder(
+              future: userInfo,
+              builder: ((context, snapshot) {
+                if (snapshot.hasError) {
+                  return Center(
+                    child: Text("Error encountered! ${snapshot.error}"),
+                  );
+                } else if (snapshot.connectionState ==
+                    ConnectionState.waiting) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else if (!snapshot.hasData) {
+                  return Center(
+                    child: Text("No Todos Found"),
+                  );
+                }
+
+                UserModel user = UserModel.fromJson(
+                    snapshot.data?.data() as Map<String, dynamic>);
+
+                return ListView(padding: EdgeInsets.only(top: 80), children: [
+                  ListTile(
+                    title: Text(
+                      'Hello!',
+                      style:
+                          TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  ListTile(
+                    title: Text(
+                      '${user.firstName!}',
+                      style:
+                          TextStyle(fontSize: 60, fontWeight: FontWeight.w100),
+                    ),
+                  ),
+                  Container(
+                    height: 300,
+                  ),
+                  ListTile(
+                      title: const Text('Shared Todo'),
+                      onTap: () {
                         Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const Profile()),
-              );
-        }),
-        ListTile(
-          title: const Text('Friends'),
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const SearchFriends()),
-            );
-          },
-        ),
-        ListTile(
-          title: const Text('Send Friend Request'),
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => const SendFriendRequest()),
-            );
-          },
-        ),
-        ListTile(
-          title: const Text('View Sent Friend Request'),
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => const SentFriendRequest()),
-            );
-          },
-        ),
-        ListTile(
-          title: const Text('Received Friends Request'),
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const FriendRequest()),
-            );
-          },
-        ),
-        ListTile(
-            title: const Text('Logout'),
-            onTap: () {
-              context.read<AuthProvider>().signOut();
-              Navigator.pop(context);
-            }),
-      ])),
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const sharedTodo()),
+                        );
+                      }),
+                  ListTile(
+                      title: const Text('Profile'),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const Profile()),
+                        );
+                      }),
+                  ListTile(
+                    title: const Text('Friends'),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const SearchFriends()),
+                      );
+                    },
+                  ),
+                  ListTile(
+                    title: const Text('Send Friend Request'),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const SendFriendRequest()),
+                      );
+                    },
+                  ),
+                  ListTile(
+                    title: const Text('View Sent Friend Request'),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const SentFriendRequest()),
+                      );
+                    },
+                  ),
+                  ListTile(
+                    title: const Text('Received Friends Request'),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const FriendRequest()),
+                      );
+                    },
+                  ),
+                  ListTile(
+                      title: const Text('Logout'),
+                      onTap: () {
+                        context.read<AuthProvider>().signOut();
+                        Navigator.pop(context);
+                      }),
+                ]);
+              }))),
       body: StreamBuilder(
         stream: todosStream,
         builder: (context, snapshot) {
