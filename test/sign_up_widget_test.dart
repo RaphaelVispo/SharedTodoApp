2302d78893bafde1ac56b500ea90aea405895d31
RaphaelVispo <rsvispo@up.edu.ph>
2022-12-13 16:26:23 +0800

    test: Widget test for sign up

--- test/sign_up_widget_test.dart
+++ test/sign_up_widget_test.dart
@@ -1,3 +1,4 @@
+import 'package:date_field/date_field.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:week7_networking_discussion/screens/signup.dart';
@@ -19,6 +20,9 @@ void main() {
       final lastwordField = find.byKey(const Key("lastnamekeysignup"));
       final signUpButton = find.byKey(const Key("signupbuttonnamekeysignup"));
       final backButton = find.byKey(const Key("backbuttonnamekeysignup"));
+      final dateTime = find.byKey(const Key("DateTimePickerKey"));
+      final bioField = find.byKey(const Key("biokeysignup"));
+
 
       // Use the `findsOneWidget` matcher provided by flutter_test to
       // verify that the Text widgets and Button widgets appear exactly once in the widget tree.
@@ -27,6 +31,8 @@ void main() {
       expect(passwordField, findsOneWidget);
       expect(firstNameField, findsOneWidget);
       expect(lastwordField, findsOneWidget);
+      expect(dateTime, findsOneWidget);
+      expect(bioField, findsOneWidget);
 
       expect(signUpButton, findsOneWidget);
       expect(backButton, findsOneWidget);
@@ -45,16 +51,26 @@ void main() {
       final passwordField = find.byKey(const Key("passwordkeysignup"));
       final firstNameField = find.byKey(const Key("firstnamekeysignup"));
       final lastwordField = find.byKey(const Key("lastnamekeysignup"));
+      final bioField = find.byKey(const Key("biokeysignup"));
+      
 
       await tester.enterText(userNameField, "rap@gmail.com");
-      await tester.enterText(passwordField, "1234567");
+      await tester.enterText(passwordField, "111111111");
       await tester.enterText(firstNameField, "Raphael");
       await tester.enterText(lastwordField, "Vispo");
+      //await tester.enter
+      await tester.enterText(bioField, "Vispo");
+
+      await tester.tap(find.byType(DateTimeFormField));
+      await tester.pump();
+      await tester.tap(find.text('1'));
+      await tester.tap(find.text("OK"));
 
       final signUpButton = find.byKey(const Key("signup"));
       final emailErrorFinder = find.text('Invalid Email');
-      final passwordErrorFinder = find.text('The length should be more than 8');
+      final passwordErrorFinder = find.text('The length should be more than 7');
       final nullError = find.text('Please enter some text');
+      final nullErrorDate = find.text('Please enter a date');
 
       await tester.tap(signUpButton);
 
@@ -62,6 +78,7 @@ void main() {
       expect(emailErrorFinder, findsNothing);
       expect(passwordErrorFinder, findsNothing);
       expect(nullError, findsNothing);
+      expect(nullErrorDate, findsNothing);
 
       // expect(find.text('Todo'), findsOneWidget);
     }));
@@ -70,11 +87,13 @@ void main() {
 
       final signUpButton = find.byKey(const Key("signup"));
       final emailErrorFinder = find.text('Please enter some text');
+      final nullErrorDate = find.text('Please enter a date');
 
       await tester.tap(signUpButton);
 
       await tester.pump(const Duration(milliseconds: 100));
       expect(emailErrorFinder, findsWidgets);
+      expect(nullErrorDate, findsWidgets);
     }));
 
     testWidgets("Error when input is invalid email and password",
@@ -85,21 +104,32 @@ void main() {
       final passwordField = find.byKey(const Key("passwordkeysignup"));
       final firstNameField = find.byKey(const Key("firstnamekeysignup"));
       final lastwordField = find.byKey(const Key("lastnamekeysignup"));
+      final bioField = find.byKey(const Key("biokeysignup"));
 
       await tester.enterText(userNameField, "rap");
       await tester.enterText(passwordField, "not");
       await tester.enterText(firstNameField, "Raphael");
       await tester.enterText(lastwordField, "Vispo");
+      await tester.enterText(bioField, "Yolo");
 
       final signUpButton = find.byKey(const Key("signup"));
       final emailErrorFinder = find.text('Invalid Email');
-      final passwordErrorFinder = find.text('The length should be more than 8');
+      final passwordErrorFinder = find.text('The length should be more than 7');
+      final invalidDate = find.text('Enter a past Date');
+      
+      await tester.tap(find.byType(DateTimeFormField));
+      await tester.pump();
+      await tester.tap(find.text('25'));
+      await tester.tap(find.text("OK"));
 
       await tester.tap(signUpButton);
 
       await tester.pump(const Duration(milliseconds: 100));
+      
       expect(emailErrorFinder, findsOneWidget);
       expect(passwordErrorFinder, findsOneWidget);
+      expect(invalidDate, findsOneWidget);
+
     }));
   }));
 }
